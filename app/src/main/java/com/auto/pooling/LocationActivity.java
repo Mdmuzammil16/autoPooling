7e5b6c4dc995b49e5c86c7d458a2a77010974e06
Mdmuzammil16 <fazil8785@gmail.com>
2024-12-10 19:28:44 +0530

    location code added

--- app/src/main/java/com/auto/pooling/LocationActivity.java
+++ app/src/main/java/com/auto/pooling/LocationActivity.java
@@ -1,15 +1,119 @@
 package com.auto.pooling;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.auto.pooling.databinding.ActivityLocationBinding;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class LocationActivity extends AppCompatActivity {
+
+    ActivityLocationBinding binding;
+    String apiKey = "AIzaSyB24Hrv9znLj4PB1Aryc7a3kd3EzwJ9dZk";
+    ArrayList<JSONObject> arrayList = new ArrayList<>();
+
+    private static final long DEBOUNCE_DELAY = 900;
+    private final Handler handler = new Handler();
+    private Runnable searchRunnable;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_location);
-        // hello
+        binding = ActivityLocationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.locationEt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                handler.removeCallbacks(searchRunnable);
+                searchRunnable = () -> performSearch(s.toString());
+                handler.postDelayed(searchRunnable, DEBOUNCE_DELAY);
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+
+    private void performSearch(String query) {
+        int radius = 10;
+        String url = makeLocationUrl(query, apiKey, radius);
+        getLocation(url);
+    }
+
+    String makeLocationUrl(
+            String searchText,
+            String apiKey,
+            int radius
+    ) {
+        String url = "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=" + searchText;
+        url += "&key=" + apiKey;
+        url += "&radius=" + radius;
+        url += "&components=country:IN";
+        return url;
+    }
+
+    private void getLocation(String url) {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                response -> {
+                    JSONObject jsonObject = null;
+                    try {
+                        jsonObject = new JSONObject(response);
+                        JSONArray predictions = jsonObject.getJSONArray("predictions");
+                        for (int i = 0; i < predictions.length(); i++) {
+                            JSONObject prediction = predictions.getJSONObject(i);
+                            arrayList.add(prediction); // Add to the ArrayList
+                        }
+                    } catch (JSONException e) {
+                        Toast.makeText(LocationActivity.this, "Error: " + e, Toast.LENGTH_LONG).show();
+
+                    }
+                },
+                error -> Toast.makeText(LocationActivity.this, "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show());
+        requestQueue.add(stringRequest);
+    }
+
+    private void getPlaceDetails(String placeId) {
+        String url = "https://maps.googleapis.com/maps/api/place/details/json?place_id=" + placeId + "&key=" + apiKey;
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                response -> {
+                    try {
+                        JSONObject jsonObject = new JSONObject(response);
+                        JSONObject result = jsonObject.getJSONObject("result");
+                        JSONObject geometry = result.getJSONObject("geometry");
+                        JSONObject location = geometry.getJSONObject("location");
+                        double latitude = location.getDouble("lat");
+                        double longitude = location.getDouble("lng");
+                        Log.d("fazilApp", "Latitude: " + latitude + ", Longitude: " + longitude);
+                        Toast.makeText(LocationActivity.this, "Latitude: " + latitude + ", Longitude: " + longitude, Toast.LENGTH_LONG).show();
+                    } catch (Exception e) {
+                        Log.d("fazilApp", "Error parsing Place Details: " + e.getMessage());
+                    }
+                },
+                error -> {
+                    Log.d("fazilApp", "Error fetching Place Details: " + error.getMessage());
+                });
+
+        requestQueue.add(stringRequest);
     }
 }
--- app/src/main/java/com/auto/pooling/LoginActivity.java
+++ app/src/main/java/com/auto/pooling/LoginActivity.java
@@ -1,14 +1,126 @@
 package com.auto.pooling;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.auto.pooling.databinding.ActivityLoginBinding;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
+
+    ActivityLoginBinding binding;
+    FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+    ProgressBar pd;
+    private ActivityResultLauncher<Intent> googleSignInLauncher;
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        pd = binding.progressBar;
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("310427835343-t2ra624m1isl5fthe6nbj16a6sc8o1gk.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        binding.googleBtn.setOnClickListener(view -> signIn());
+
+        googleSignInLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+                        Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(result.getData());
+                        try {
+                            GoogleSignInAccount account = task.getResult(ApiException.class);
+                            firebaseAuthWithGoogle(account.getIdToken());
+                        } catch (ApiException e) {
+                            Toast.makeText(getApplicationContext(), "Failed: " + e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Sign-In canceled or failed", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+
+
     }
+
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        binding.googleBtn.setVisibility(View.GONE);
+        pd.setVisibility(View.VISIBLE);
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this, task -> {
+            try {
+                if (task.isSuccessful()) {
+
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    db = FirebaseFirestore.getInstance();
+                    Map<String, Object> users = new HashMap<>();
+                    users.put("uid", user.getUid() + "");
+                    users.put("name", user.getDisplayName());
+                    users.put("email", user.getEmail());
+
+                    db.collection("users").document("" + user.getUid()).set(users, SetOptions.merge())
+                            .addOnSuccessListener(a -> {
+                                pd.setVisibility(View.GONE);
+                                Toast.makeText(getApplicationContext(), "User Created Successfully", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(this, MainPageActivity.class));
+                                finish();
+                            })
+                            .addOnFailureListener(e -> {
+                                Toast.makeText(getApplicationContext(), "failed" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                pd.setVisibility(View.GONE);
+                                binding.googleBtn.setVisibility(View.VISIBLE);
+                            });
+                } else {
+                    Toast.makeText(getApplicationContext(), "Failed " + task.getException(), Toast.LENGTH_SHORT).show();
+                    pd.setVisibility(View.INVISIBLE);
+                    binding.googleBtn.setVisibility(View.VISIBLE);
+                }
+            } catch (Exception e) {
+                Toast.makeText(this, "Exception " + e, Toast.LENGTH_SHORT).show();
+                pd.setVisibility(View.INVISIBLE);
+            }
+
+        });
+
+
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        googleSignInLauncher.launch(signInIntent);
+    }
+
 }
